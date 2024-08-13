b3255f950e23f4ee2b566494f6a38f065f7b929d
Myo Thiha <51917190+MyoThiha987@users.noreply.github.com>
2024-08-13 07:40:53 +0700

    delete sensitive info

--- composeApp/src/commonMain/kotlin/com/myothiha/compose/movie/data/Token.kt
+++
@@ -1,8 +0,0 @@
-package com.myothiha.compose.movie.data
-
-/**
- * @Author Liam
- * Created at 13/Aug/2024
- */
-
-const val CLIENT_TOKEN = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MWEzMWQzNWUwYTMyNzg1ZjJlNGM0NDk5ZjA0M2FlOCIsInN1YiI6IjVkYzM5NDBjOWQ4OTM5MDAxODM0YjVlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Eve0FD4yriTnRWsCD0P2bTXplUlUObIIfs1Q5ChAdgc"
